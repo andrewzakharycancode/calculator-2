769b643deecbfb465f8836caaa58ded57d820609
Bianca M <matthews.biancam@gmail.com>
2023-03-01 15:17:17 -0600

    Got + and - working

--- calculator.py
+++ calculator.py
@@ -8,4 +8,26 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 userInput = ['']
 while userInput[0] != 'q':
     userInput = input('> ').split(' ')
+    # userInput is always going to be a list
+    # the first member of userInput (which is userInput[0]) is always going to be the operator (+, -, *, /, mod, pow, square, cube)
+    # next two members of userInput is going to be num1 and num2
     print(userInput)
+    num1 = int(userInput[1])
+    num2 = int(userInput[2])
+    if userInput[0] == '+':
+        result = add(num1, num2)
+    elif userInput[0] == '-':
+        result = subtract(num1, num2)
+    #elif userInput[0] == '*':   
+        ## call multiply
+    #elif userInput[0] == '/':
+        ## call divide
+    #elif userInput[0] == 'square':
+        ## call square
+    #elif userInput[0] == 'cube':
+        ## call cube
+    #elif userInput[0] == 'power':
+        ## call power
+    #elif userInput[0] == 'mod':
+        ## call mod
+    print(result)
