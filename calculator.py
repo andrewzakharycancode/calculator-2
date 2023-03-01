919f30673b083164946387a9140d21d32ba63f21
Bianca M <matthews.biancam@gmail.com>
2023-03-01 15:32:30 -0600

    Got *, /, square, cube, power, mod working

--- calculator.py
+++ calculator.py
@@ -11,23 +11,28 @@ while userInput[0] != 'q':
     # userInput is always going to be a list
     # the first member of userInput (which is userInput[0]) is always going to be the operator (+, -, *, /, mod, pow, square, cube)
     # next two members of userInput is going to be num1 and num2
-    print(userInput)
+    print(f"user input tokenized: {userInput}")
     num1 = int(userInput[1])
-    num2 = int(userInput[2])
+    
+    try:
+        num2 = int(userInput[2])
+    except:
+        num2 = 0
+    print(f"num2={num2}") 
     if userInput[0] == '+':
         result = add(num1, num2)
     elif userInput[0] == '-':
         result = subtract(num1, num2)
-    #elif userInput[0] == '*':   
-        ## call multiply
-    #elif userInput[0] == '/':
-        ## call divide
-    #elif userInput[0] == 'square':
-        ## call square
-    #elif userInput[0] == 'cube':
-        ## call cube
-    #elif userInput[0] == 'power':
-        ## call power
-    #elif userInput[0] == 'mod':
-        ## call mod
+    elif userInput[0] == '*':   
+        result = multiply(num1, num2)
+    elif userInput[0] == '/':
+        result = divide(num1, num2)
+    elif userInput[0] == 'square':
+        result = square(num1)
+    elif userInput[0] == 'cube':
+        result = cube(num1)
+    elif userInput[0] == 'power':
+        result = power(num1, num2)
+    elif userInput[0] == 'mod':
+        result = mod(num1, num2)
     print(result)
